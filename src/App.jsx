1eb328fabc30761699041dcc8ddd2d6cbb410fbb
TahaKhan-087 <khanmuhammadtaha087@gmail.com>
2025-05-30 10:50:16 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ export default function App() {
 
   return (
     <div className="container">
-      <h1>Simple React CRUD FORM</h1>
+      <h1>Simple CURD Project</h1>
       <form onSubmit={handleSubmit} className="form">
         <input
           type="text"
